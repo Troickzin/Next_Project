1ba0917f9eaae4360f81babcc523c704cb5bd037
Troickzin <troickzin@gmail.com>
2024-10-22 17:51:47 -0300

    teste com novo layout da rota github

--- src/app/github/[id]/page.jsx
+++ src/app/github/[id]/page.jsx
@@ -8,25 +8,49 @@ export async function getUserGithubAPI(user) {
   return posts;
 }
 
+export async function getGithubOrgs(user) {
+  let data = await fetch(`https://api.github.com/users/${user}/orgs`);
+  let posts = await data.json();
+  return posts;
+}
+
 export default async function Page({ params }) {
   const param = await params;
   const user = await getUserGithubAPI(param.id);
+  const user_orgs = await getGithubOrgs(param.id);
+
   return (
     <>
       <div className="User_Profile">
-        <pre>{user.message}</pre>
-        <img
-          src={user.avatar_url}
-          width={100}
-          height={100}
-          style={{ borderRadius: "50%" }}
-          className="User_Avatar"
-        ></img>
-        <p className="User_Name">{user.name}</p>
-        <p className="User_Bio">{user.bio}</p>
-        <Link className="User_Button" href={user.html_url} target="_blank">
+        <div className="Header" />
+        <div className="Infos">
+          <img
+            src={user.avatar_url}
+            width={200}
+            height={200}
+            style={{ borderRadius: "50%" }}
+            className="User_Avatar"
+          ></img>
+          <p className="User_Name">{user.name}</p>
+          <p className="Follows">
+            {user.followers + " Follower ·" + user.following + " Following"}
+          </p>
+          <p className="User_Bio">{user.bio}</p>
+        </div>
+        <div className="Orgs">
+          {user_orgs ? <p>Organizations</p> : ""}
+          <div className="Organizations_Content">
+            {user_orgs
+              ? user_orgs.map((e) => {
+                  return <img src={e.avatar_url} width={40} height={40} />;
+                })
+              : ""}
+          </div>
+        </div>
+
+        {/* <Link className="User_Button" href={user.html_url} target="_blank">
           <GrFormNextLink className="Icon" />
-        </Link>
+        </Link> */}
       </div>
     </>
   );
