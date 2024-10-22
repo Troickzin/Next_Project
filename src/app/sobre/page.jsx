16ca6344bfa26e25d3e64965359eb106a5b2338a
Troickzin <troickzin@gmail.com>
2024-10-21 21:17:58 -0300

    Primeiro commit de um projeto para estudar Next.JS

---
+++ src/app/sobre/page.jsx
@@ -0,0 +1,9 @@
+import style from "../page.module.css";
+
+export default function Sobre() {
+  return (
+    <div className={style.page}>
+      <h1>Sobre</h1>
+    </div>
+  );
+}
---
+++ src/app/teste/page.jsx
@@ -0,0 +1,9 @@
+import style from "../page.module.css";
+
+export default function Teste() {
+  return (
+    <div className={style.page}>
+      <h1>Teste</h1>
+    </div>
+  );
+}
